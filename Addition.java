7e6cb47a7539272c7b3a3a1c41cc9382b5422cab
ADMIN <ADMIN@DESKTOP-HU1ADE2>
2022-10-04 15:08:05 +0530

    xyz

---
+++ Addition.java
@@ -0,0 +1,13 @@
+package sunil;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+int add;
+add=i+10;
+System.out.println(add);
+	}
+
+}
---
+++ Substraction.java
@@ -0,0 +1,11 @@
+package sunil;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Substraction");
+
+	}
+
+}
